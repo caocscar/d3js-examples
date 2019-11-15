acb0cdb44f6006790f08b3c5ee8becc4b4df437f
Alex Cao <caoa@umich.edu>
2019-11-15 15:50:01 -0500

    Updated D3 workshop material

--- bar/sortable_timeseries/sortable.js
+++ bar/sortable_timeseries/sortable.js
@@ -1,22 +1,17 @@
 async function readData() {
     
+  // read data
+  const fileLocation = 'https://gist.githubusercontent.com/caocscar/8cdb75721ea4f6c8a032a00ebc73516c/raw/854bbee2faffb4f6947b6b6c2424b18ca5a8970e/mlb2018.csv'
+  DATA = await d3.csv(fileLocation, type)
+  chartDate = new Date(2018,3,3)
+  data = filterData(chartDate)
+  
+  // margins
   let margin = {top: 80, right: 90, bottom: 30+50, left: 120},
     width = 900 - margin.left - margin.right,
     height = 1500 - margin.top - margin.bottom; // 760
 
-  y = d3.scaleBand()
-      .range([height, 0])
-      .padding(0.33)
-
-  x = d3.scaleLinear()
-      .range([0, width]);
-
-  let xAxis = d3.axisTop(x)
-      .ticks(6)
-
-  let yAxis = d3.axisLeft(y)
-      .tickFormat('')
-
+  // svg setup
   let svg = d3.select('body').append('svg')
       .attr("class", "chart")
       .attr("width", width + margin.left + margin.right)
@@ -24,17 +19,26 @@ async function readData() {
     .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const fileLocation = 'https://gist.githubusercontent.com/caocscar/8cdb75721ea4f6c8a032a00ebc73516c/raw/854bbee2faffb4f6947b6b6c2424b18ca5a8970e/mlb2018.csv'
-  DATA = await d3.csv(fileLocation, type)
-  chartDate = new Date(2018,3,3)
-  data = filterData(chartDate)
-  y.domain(data.map(d => d.team).reverse());
-  x.domain([0, Math.ceil(d3.max(data, d => d.value)/5)*5]);
+  // set up scales
+  let y = d3.scaleBand()
+      .domain(data.map(d => d.team).reverse())
+      .range([height, 0])
+      .padding(0.33)
 
+  let x = d3.scaleLinear()
+      .domain([0, Math.ceil(d3.max(data, d => d.value)/5)*5])
+      .range([0, width]);
+      
   // add axes
+  let xAxis = d3.axisTop(x)
+      .ticks(6)
+
   svg.append("g")
       .attr("class", "x axis")
       .call(xAxis);
+
+  let yAxis = d3.axisLeft(y)
+      .tickFormat('')
 
   svg.append("g")
       .attr("class", "y axis")
@@ -102,21 +106,27 @@ async function readData() {
       .text('Teams')
 
   // date label
-  const formatDate = d3.timeFormat('%b %-d, %Y')
+  const formatDate = d3.timeFormat('%b %-d')
   let dateLabel = labels.append('text')
       .attr('id', 'date')
-      .attr('transform', `translate(0,-40)`)
+      .attr('transform', 'translate(0,-40)')
       .text(formatDate(chartDate))
 
-  // clipping rectangle; not sure why it doesn't match the area clipped
+  labels.append('text')
+      .attr('id', 'season')
+      .attr('transform', `translate(${width/2},-40)`)
+      .text('MLB 2018 Season')
+
+  // clipping rectangle
   const z = 0.97*(height / data.length)
-  svg.append("defs").append("clipPath")
+  d3.select('.chart').append("defs")
+    .append("clipPath")
       .attr("id", "clip")
     .append("rect")
-      .attr('x', -imgsize-10)
+      .attr('x', 0)
       .attr('y', 0)
-      .attr("width", width+300)
-      .attr("height", height-z+85)    
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", 0.4*height)    
 
   // sorting transition
   const T = 300
@@ -124,7 +134,7 @@ async function readData() {
 
     chartDate = d3.timeDay.offset(chartDate,1)
     dateLabel.transition().duration(T)
-        .text(`${formatDate(chartDate)}   MLB 2018 Season`)
+        .text(formatDate(chartDate))
     data = filterData(chartDate)
 
     // update x-axis
