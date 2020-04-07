6d67c65091f3fa70b012a2c90b11cf5ad2bb92da
Alex <caoa@umich.edu>
2020-04-07 10:14:31 -0400

    Added radio selection to hexmap

--- maps/hexmap/hexmap.js
+++ maps/hexmap/hexmap.js
@@ -13,17 +13,17 @@ let svg = d3.select("#map")
   .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`)
 
-const intervals = [10,100,1000,10000,100000]
+let intervals = [100,1000,10000,100000]
 let colorScale = d3.scaleThreshold()
     .domain(intervals)
-    .range(d3.schemeYlGnBu[6])
+    .range(d3.schemeYlOrBr[5])
 
 // Legend
 const xLegend = 20
 const yLegend = 0.65*height
 const side = 20
-const legendIntervals = [0].concat(intervals)
-const legendFmt = d3.format(',')
+let legendIntervals = [0].concat(intervals)
+const legendFmt = d3.format(',.3~f')
 let legend = svg.selectAll(".legend")
   .data(legendIntervals)
   .join("g")
@@ -43,11 +43,14 @@ legend.append("text")
     .text((d,i,arr) => i < legendIntervals.length-1 ? `${legendFmt(d)}-${legendFmt(arr[i+1].__data__ - 1)}` : `${legendFmt(d)}+`);
 
 svg.append("text")
-    .attr("class", "legendLabel")
+    .attr("id", "legendTitle")
     .attr("x", xLegend)
     .attr("y", yLegend)
     .attr("dy", ".35em")
     .text("Population");
+
+let column = 'POP';
+const radioButton = d3.selectAll('input')
 
 // Load map data
 async function makeHexMap(filename) {
@@ -66,17 +69,52 @@ async function makeHexMap(filename) {
       .join("path")
         .attr('class', 'hex')
         .attr("d", path)
-        .attr("fill", d => colorScale(d.properties.POP))
+        .attr("fill", d => colorScale(d.properties[column]))
         .attr("data-toggle", "tooltip")
         .attr("data-html", true)
         .attr("title", d => {
-          txt = `Pop: ${d3.format(',')(d.properties.POP)}`
+          txt = `index: ${d.properties.index}`
+          txt += `<br>Pop: ${d3.format(',')(d.properties.POP)}`
           txt += `<br>Cases: ${d.properties.Cases}`
-          txt += `<br>Case Rate: ${d3.format('.6f')(d.properties.RawCsRt)}`
+          txt += `<br>Case Rate: ${d3.format('.3f')(10000*d.properties.RawCsRt)}`
           return txt
         })
 
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    radioButton.on('change', function(d) {
+        column = this.value
+        if (column === 'POP') {
+            intervals = [100,1000,10000,100000]
+            colorScale.range(d3.schemeYlOrBr[5])
+            text = 'Population'
+        } else if (column === 'Cases') {
+            intervals = [1,10,100,1000]
+            colorScale.range(d3.schemePuBuGn[5])
+            text = 'Cases'
+        } else {
+            intervals = [1, 2, 5, 10]
+            colorScale.range(d3.schemeRdPu[5])
+            text = 'Case Rate'
+        }
+        colorScale.domain(intervals)
+        svg.selectAll('.hex')
+          .transition().duration(500)
+            .delay((d,i) => i*5)
+            .style('fill', d => column == 'RawCsRt' ? colorScale(10000*d.properties[column]) : colorScale(d.properties[column]))
+        svg.select('#legendTitle')
+            .text(text)
+        legendIntervals = [0].concat(intervals)
+        svg.selectAll('rect')
+          .data(legendIntervals)
+            .style('fill', d => colorScale(d))
+        svg.selectAll('.legendLabel')
+          .data(legendIntervals)
+            .text((d,i,arr) => {
+                offset = column == 'RawCsRt' ? 0 : 1
+                return i < legendIntervals.length-1 ? `${legendFmt(d)}-${legendFmt(arr[i+1].__data__ - offset)}` : `${legendFmt(d)}+`
+            });   
     })
 };
